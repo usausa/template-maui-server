366bd06a6c77bd2c72cc8934b0f6a9f2e929022a
usausa <machi.pon@gmail.com>
2023-07-17 10:32:57 +0900

    Update baseline

---
+++ Template.MobileServer.Tests/GlobalUsing.cs
@@ -0,0 +1,47 @@
+// ReSharper disable RedundantUsingDirective.Global
+#pragma warning disable
+global using System;
+global using System.Buffers;
+global using System.Collections;
+global using System.Collections.Generic;
+global using System.ComponentModel;
+global using System.ComponentModel.DataAnnotations;
+global using System.Data;
+global using System.Data.Common;
+global using System.Diagnostics.CodeAnalysis;
+global using System.Globalization;
+global using System.IO;
+global using System.Linq;
+global using System.Net;
+global using System.Net.Http;
+global using System.Runtime.CompilerServices;
+global using System.Text;
+global using System.Threading;
+global using System.Threading.Tasks;
+
+global using AutoMapper;
+
+// TODO
+global using Bunit;
+
+global using Microsoft.AspNetCore.Authentication;
+global using Microsoft.AspNetCore.Authorization;
+global using Microsoft.AspNetCore.Http;
+global using Microsoft.AspNetCore.Mvc;
+
+global using Microsoft.Extensions.Logging;
+
+global using Smart;
+global using Smart.Collections.Generic;
+global using Smart.Text;
+
+// TODO
+global using Template.MobileServer;
+global using Template.MobileServer.Domain;
+//global using Template.MobileServer.Models.Connector;
+global using Template.MobileServer.Models.Entity;
+//global using Template.MobileServer.Models.Paging;
+//global using Template.MobileServer.Models.View;
+//global using Template.MobileServer.Services;
+
+global using Xunit;
