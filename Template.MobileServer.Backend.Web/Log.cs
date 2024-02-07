93eeab0a9724418a85a5261c37cc7dcc7f24573c
usausa <machi.pon@gmail.com>
2024-02-07 14:06:48 +0900

    Update message

--- Template.MobileServer.Backend.Web/Log.cs
+++ Template.MobileServer.Backend.Web/Log.cs
@@ -7,13 +7,13 @@ internal static partial class Log
     [LoggerMessage(Level = LogLevel.Information, Message = "Service start.")]
     public static partial void InfoServiceStart(this ILogger logger);
 
-    [LoggerMessage(Level = LogLevel.Information, Message = "Environment. version=[{version}], runtime=[{runtime}], directory=[{directory}]")]
+    [LoggerMessage(Level = LogLevel.Information, Message = "Environment: version=[{version}], runtime=[{runtime}], directory=[{directory}]")]
     public static partial void InfoServiceSettingsEnvironment(this ILogger logger, Version? version, Version runtime, string directory);
 
-    [LoggerMessage(Level = LogLevel.Information, Message = "GCSettings. serverGC=[{isServerGC}], latencyMode=[{latencyMode}], largeObjectHeapCompactionMode=[{largeObjectHeapCompactionMode}]")]
+    [LoggerMessage(Level = LogLevel.Information, Message = "GCSettings: serverGC=[{isServerGC}], latencyMode=[{latencyMode}], largeObjectHeapCompactionMode=[{largeObjectHeapCompactionMode}]")]
     public static partial void InfoServiceSettingsGC(this ILogger logger, bool isServerGC, GCLatencyMode latencyMode, GCLargeObjectHeapCompactionMode largeObjectHeapCompactionMode);
 
-    [LoggerMessage(Level = LogLevel.Information, Message = "ThreadPool. workerThreads=[{workerThreads}], completionPortThreads=[{completionPortThreads}]")]
+    [LoggerMessage(Level = LogLevel.Information, Message = "ThreadPool: workerThreads=[{workerThreads}], completionPortThreads=[{completionPortThreads}]")]
     public static partial void InfoServiceSettingsThreadPool(this ILogger logger, int workerThreads, int completionPortThreads);
 
     // Error
