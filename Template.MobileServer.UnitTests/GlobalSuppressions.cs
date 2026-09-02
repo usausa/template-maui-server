b6b1d90bf8ed718655365fea61b158f2f1bd289f
usausa <machi.pon@gmail.com>
2026-09-02 14:38:02 +0900

    Update

--- Template.MobileServer.UnitTests/GlobalSuppressions.cs
+++ Template.MobileServer.UnitTests/GlobalSuppressions.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics.CodeAnalysis;
 
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1034:NestedTypesShouldNotBeVisible", Justification = "Ignore")]
 [assembly: SuppressMessage("Maintainability", "CA1515:Consider making public types internal", Justification = "Ignore")]
 [assembly: SuppressMessage("Reliability", "CA2007:DoNotDirectlyAwaitATaskAnalyzer", Justification = "Ignore")]
--- Template.MobileServer.Web/Program.cs
+++ Template.MobileServer.Web/Program.cs
@@ -84,6 +84,4 @@ await app.InitializeApplicationAsync();
 await app.RunAsync();
 
 [ExcludeFromCodeCoverage]
-public partial class Program
-{
-}
+public partial class Program;
